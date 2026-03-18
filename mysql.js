4d2565d6c159ff21a3ee38dc68cdcf72544435ba
Kathy Yeung <kathyyeung7@gmail.com>
2026-03-18 16:50:03 -0400

    fixed server to use JAWSDB_URL on Heroku

--- mysql.js
+++ mysql.js
@@ -1,16 +1,24 @@
 import mysql from 'mysql2';
-import 'dotenv/config';
+import dotenv from 'dotenv';
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+dotenv.config();
 
-const promisePool = pool.promise();
+let pool;
 
-export default promisePool;
+if (process.env.JAWSDB_URL) {
+    pool = mysql.createPool(process.env.JAWSDB_URL);
+} else {
+    pool = mysql.createPool({
+        host: process.env.DB_HOST || '127.0.0.1',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    });
+}
+
+const db = pool.promise();
+
+export default db;
