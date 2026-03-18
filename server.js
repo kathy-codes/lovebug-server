5aae2e6b057fbce491a8b98902c9f76bec4cc5d3
Kathy Yeung <kathyyeung7@gmail.com>
2026-03-18 19:00:45 -0400

    updated origins for testing

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ const port = process.env.PORT;
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://lovebug-frontend.netlify.app']
+    origin: [
+        'http://localhost:5173', 
+        'http://localhost:5174',
+        'https://lovebug-frontend.netlify.app'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
