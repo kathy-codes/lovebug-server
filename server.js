b22cce71f20b3503f7ff020f5f5ec2357be1d729
Kathy Yeung <kathyyeung7@gmail.com>
2026-03-18 16:42:29 -0400

    updated db to match frontend for deployment

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ import matchRoutes from "./routes/matchRoutes.js";
 const port = process.env.PORT;
 const app = express();
 
-app.use(cors()); 
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://kathy-codes.github.io']
+}));
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
